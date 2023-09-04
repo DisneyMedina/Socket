bee7b7a2aa0762a752c3ae076ff563585d1f94d1
dmedina <dmedina@bytesw.com>
2023-09-03 21:55:48 -0500

    Initial commit

---
+++ src/main/java/com/cibertec/socket/Cibertec/ClientSocket.java
@@ -0,0 +1,13 @@
+package com.cibertec.socket.Cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSocket {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSocket.class, args);
+	}
+
+}
---
+++ src/main/java/com/cibertec/socket/Cibertec/ServerSocket.java
@@ -0,0 +1,8 @@
+package com.cibertec.socket.Cibertec;
+
+public class ServerSocket {
+
+    public static void main(String[] args) {
+
+    }
+}
